45e8b870d5630da8a8d79e0d5e554ad8fb47e9b5
ochkin <ochkin@gmail.com>
2015-11-29 01:17:09 +0800

    Problem 63.

---
+++ ProjectEulerFs/Util.fs
@@ -0,0 +1,7 @@
+﻿module Util
+
+let numberOfDigits n =
+    if n < 0I then
+      invalidArg "n" "Negative"
+    else
+      n.ToString().Length
---
+++ ProjectEulerFs/prb63.fs
@@ -0,0 +1,23 @@
+﻿module prb63
+
+//let ns (digit:byte) =
+//    let limit = 1.0 / (1.0 - log10(float digit))
+//    Seq.initInfinite (fun i -> i)
+//        |> Seq.skipWhile (fun i -> i < 1)
+//        |> Seq.takeWhile (fun i -> float i <= limit)
+let find (digit:byte) =
+    let limit = 22.0 //1.0 / (1.0 - log10(float digit))
+    let rec findRec n acc = seq {
+        if float n <= limit then
+            yield (n, acc)
+            yield! findRec (n+1) (acc* (int digit |> bigint))
+    }
+    findRec 1 (int digit |> bigint)
+let correct (n, power) =
+    if n = (power |> Util.numberOfDigits) then
+        //printfn "%A is %i digits" power n
+        true
+    else
+        false
+let solve () =
+    [1uy .. 9uy] |> Seq.collect find |> Seq.where correct |> Seq.length
