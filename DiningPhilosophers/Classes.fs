f69df4bdd92bc80c7fde974384f8c141c212990f
ochkin <ochkin@gmail.com>
2016-06-18 22:51:22 +0800

    Dining philosophers - dirst draft (semaphore).

---
+++ DiningPhilosophers/Classes.fs
@@ -0,0 +1,31 @@
+﻿namespace Classes
+
+open System.Threading
+open System
+
+type Fork () =
+    let mutable takenBy = None
+    member x.Available with get() = Option.isNone takenBy
+    member x.Take (person) =
+        ignore <| Interlocked.CompareExchange<Philosopher option>(&takenBy, Some person, None)
+        takenBy = Some person
+    member x.Release (person) =
+        ignore <| Interlocked.CompareExchange<Philosopher option>(&takenBy, None, Some person)
+        //x.Available
+        
+and Philosopher(name: string, leftFork: Fork, rightFork: Fork, eatingTime: TimeSpan) =
+    member x.Name = name
+    member x.CanEat = leftFork.Available && rightFork.Available
+    member x.Eat () = async {
+        if leftFork.Take x && rightFork.Take x then
+            printfn "%s is eating for %A..." name eatingTime
+            do! Async.Sleep (int eatingTime.TotalMilliseconds)
+            printfn "%s is done eating." name
+            leftFork.Release x
+            rightFork.Release x
+            return true
+        else
+            leftFork.Release x
+            rightFork.Release x
+            return false
+        }
---
+++ DiningPhilosophers/Program.fs
@@ -0,0 +1,29 @@
+﻿open Classes
+open System
+open System.Threading
+
+[<EntryPoint>]
+let main argv =
+    let N = 5
+    let forks = List.init N (fun i -> new Fork())
+    let philosophers =
+        List.pairwise (List.last forks :: forks)
+            |> List.mapi (fun i pairOfForks -> new Philosopher("P"+i.ToString(),
+                                                fst pairOfForks,
+                                                snd pairOfForks,
+                                                TimeSpan.FromSeconds(3.0)))
+    printfn "%i philosophers dining." (List.length philosophers)
+    use semaphor = new System.Threading.SemaphoreSlim(2, 2)
+    let runPhilosopher (person: Philosopher) = async  {
+        while true do
+            do! (semaphor.WaitAsync() |> Async.AwaitTask)
+            do! (Async.Ignore (person.Eat()))
+            semaphor.Release() |> ignore
+        }
+//    let token = new CancellationToken()
+    philosophers |> Seq.map runPhilosopher |> Async.Parallel |> Async.Ignore |> Async.Start
+//    Async.Start(run, token)
+    Console.ReadKey () |> ignore
+    Async.CancelDefaultToken ()
+
+    0 // return an integer exit code
