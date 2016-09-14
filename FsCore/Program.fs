2876b18a06a6e1eb9cc1a01ec2c1205be398809d
ochkin <ochkin@gmail.com>
2016-09-14 22:20:00 +0800

    Finished replacing legacy F# .NET 4.6 project with .NET Core.

--- FsCore/Program.fs
+++ FsCore/Program.fs
@@ -5,3 +5,14 @@ let main argv =
     printfn "%A" (prb74.solve())
     Console.ReadKey() |> ignore
     0 // return an integer exit code
+
+(*
+
+    let start = System.DateTime.Now
+    printfn "%A" (prb73.solve ())
+    let finish = System.DateTime.Now
+    printfn "%A" (finish - start)
+
+    System.Console.Beep ()
+
+*)
